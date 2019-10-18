ae48b4325f475c61d0e33d778be016eed822ad5a
Pinkcherish98 <min16002@byui.edu>
2019-10-17 20:03:24 -0600

    wk5

--- assignments/lesson5/scripts/forecast.js
+++ assignments/lesson5/scripts/forecast.js
@@ -1,8 +1,11 @@
-var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-for (i = 1; i < 6; i++) {
-    if ([currentDate.getDay() + (i - 1)] <= 6) {
-        document.getElementById("day" + i).innerHTML = day[currentDate.getDay() + (i - 1)]
-    } else {
-        document.getElementById("day" + i).innerHTML = day[currentDate.getDay() + (i - 1) - 7];
-    }
-}
+/* GET DATE */
+var currentDate = new Date();
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+document.getElementById("day1").innerHTML = days[currentDate.getDay()+1];
+document.getElementById("day2").innerHTML = days[currentDate.getDay()+2];
+document.getElementById("day3").innerHTML = days[currentDate.getDay()+3];
+document.getElementById("day4").innerHTML = days[currentDate.getDay()+4];
+document.getElementById("day5").innerHTML = days[currentDate.getDay()+5];
+
+/* GET TEMPERATURE */
