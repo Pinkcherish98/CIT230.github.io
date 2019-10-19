d5fdf8485157b0a53776c525c925f3b0f5b130cf
Pinkcherish98 <min16002@byui.edu>
2019-10-19 13:42:59 -0600

    wk5

--- assignments/lesson5/scripts/forecast.js
+++ assignments/lesson5/scripts/forecast.js
@@ -1,11 +1,42 @@
-/* GET DATE */
+
 var currentDate = new Date();
-var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var currentYear = currentDate.getFullYear();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var days2 = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-document.getElementById("day1").innerHTML = days[currentDate.getDay()+1];
-document.getElementById("day2").innerHTML = days[currentDate.getDay()+2];
-document.getElementById("day3").innerHTML = days[currentDate.getDay()+3];
-document.getElementById("day4").innerHTML = days[currentDate.getDay()+4];
-document.getElementById("day5").innerHTML = days[currentDate.getDay()+5];
+// Inject to DOM
+document.getElementById("currentDate").innerHTML = days[currentDate.getDay()] + ", " + months[currentDate.getMonth()] + " " + currentDate.getDate() + ", " + currentYear;
 
-/* GET TEMPERATURE */
+// ToggleMenu
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+// Create the five-day forecast
+var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+for (i = 1; i < 6; i++) {
+    if ([currentDate.getDay() + (i - 1)] <= 6) {
+        document.getElementById("day" + i).innerHTML = day[currentDate.getDay() + (i - 1)]
+    } else {
+        document.getElementById("day" + i).innerHTML = day[currentDate.getDay() + (i - 1) - 7];
+    }
+}
+
+let currentDay = weekDayNumber;
+
+for (let i = 1; i<6; i++) {
+    //add one day to current day
+    currentDay++;
+
+    //if current day is greater than 6 (Saturday), rest it to 0 (Sunday)
+    if (currentDay > 6){
+        currentDay = 0;
+    }
+    //assign value to document.getElementById('day'+i);
+    element.innerHTML = daysOfWeek[currentDay];
+
+}
+//show hide pancake
+if (weekDayNumber ===5) {
+    document.getElementById("pancake").removeAttribute("class","hidden");
+}
