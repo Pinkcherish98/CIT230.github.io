20c3ecba972316cbdc811a10184302c68dbe3966
Pinkcherish98 <min16002@byui.edu>
2019-11-02 20:34:21 -0600

    wk7

--- assignments/lesson7/scripts/lazyload.js
+++ assignments/lesson7/scripts/lazyload.js
@@ -1,31 +1,10 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const imgOption = {
-    threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
-};
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
-
-if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    });
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
