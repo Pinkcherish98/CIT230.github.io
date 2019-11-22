f0072d406d2a0ea286c2fb63095447a401ba8b77
Pinkcherish98 <min16002@byui.edu>
2019-11-21 22:47:12 -0700

    wk10

--- assignments/lesson10/api-test/weatherapi.js
+++ assignments/lesson10/api-test/weatherapi.js
@@ -1,28 +1,26 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0b631fd8b3c62ec024dcf7737ccf66cf&units=imperial';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=1b5c7ece6b42d7f1888812029b36426b&units=imperial';
+fetch(apiUrl)
+  .then(
+    (response) => 
+      response.json()
+  )
+  .then(
+    (currentWeather)=>{
+      console.log(currentWeather.main.temp);
 
-fetch(apiURL)
-.then(
-    (response) => response.json()
-)
-.then(
-    (currentWeather) => {
-        //DEBUG:
-        console.log(currentWeather);
-        
-        let currentTemp = currentWeather.main.temp
-        let currentImageIcon = currentWeather.weather[0].icon;
-        let currentImage = 'https://openweathermap.org/img/w/' + 
-            currentImageIcon + 
-            '.png';
+     let currentTemp = currentWeather.main.temp;
+     let currentImageIcon = currentWeather.weather[0].icon;
 
-        document.querySelector('#current-temp')
-            .textContent = currentTemp;
-        document.querySelector('#imagesrc')
-            .textContent = currentImage;
-        document.querySelector('#weather-icon')
-            .setAttribute('src', currentImage);
-        
+     let currentImage = 'https://openweathermap.org/img/w/' + currentImageIcon + '.png';
+      
+      document.querySelector('#current-temp').textContent=currentTemp;
+      document.querySelector('#imagesrc').textContent=currentImage;
+      document.querySelector('#weather-icon').setAttribute('src',currentImage);
+      document.querySelector('#weather-icon').setAttribute('alt',currentWeather).weather[0].main;
 
 
+      
     }
-);
+  );
+
+
